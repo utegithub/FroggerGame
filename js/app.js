f3f36a7f8132f280fe2a1ea227747f949263c7e1
utegithub <utegithub@gmail.com>
2016-01-23 13:41:59 -0800

    Updated after review

--- js/app.js
+++ js/app.js
@@ -1,177 +1,164 @@
- //Enemy constructor
- var Enemy = function(x, y, speed) {
+//Enemy constructor
+var Enemy = function(x, y, speed) {
 
-     this.sprite = 'images/enemy-bug.png';
-     this.y = y;
-     this.speed = speed;
-     this.x = -3;
- };
- //Creating a random Speed out of the array
+    this.sprite = 'images/enemy-bug.png';
+    this.y = y;
+    this.speed = speed;
+    this.x = -3;
+};
+//Creating a random Speed out of the array
 
- function makeRandomSpeed() {
+function makeRandomSpeed() {
 
-     var speedArray = [30, 45, 55, 60, 90, 120];
-     var randSpeed = speedArray[Math.floor(Math.random() * speedArray.length)];
-     return randSpeed;
- }
-
-
- function makeRandomY() {
-
-     var yArray = [60, 145, 230];
-     var randY = yArray[Math.floor(Math.random() * yArray.length)];
-     return randY;
- }
+    var speedArray = [30, 45, 55, 60, 90, 120];
+    var randSpeed = speedArray[Math.floor(Math.random() * speedArray.length)];
+    return randSpeed;
+}
 
 
- //Updating enemy if enemy crosses the screen
- Enemy.prototype.update = function(dt) {
+function makeRandomY() {
 
-     this.x += this.speed * dt;
-     if (this.x > 600) {
-         this.reset();
-     }
- };
-
- //Resetting the enemy with the random values
- Enemy.prototype.reset = function() {
-     this.x = -2;
-     this.y = makeRandomY();
-     this.speed = makeRandomSpeed();
- };
+    var yArray = [60, 145, 230];
+    var randY = yArray[Math.floor(Math.random() * yArray.length)];
+    return randY;
+}
 
 
+//Updating enemy if enemy crosses the screen
+Enemy.prototype.update = function(dt) {
 
- //Updating enemy if enemy crosses the screen
- Enemy.prototype.update = function(dt) {
-     this.x += this.speed * dt;
-     if (this.x > 600)
+    this.x += this.speed * dt;
+    if (this.x > 600) {
+        this.reset();
+    }
+};
 
-     {
-         this.reset();
-     }
- };
+//Resetting the enemy with the random values
+Enemy.prototype.reset = function() {
+    this.x = -2;
+    this.y = makeRandomY();
+    this.speed = makeRandomSpeed();
+};
 
 
 
- // Draw the enemy on the screen, required method for game
- Enemy.prototype.render = function() {
-     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
- };
+//Updating enemy if enemy crosses the screen
+Enemy.prototype.update = function(dt) {
+    this.x += this.speed * dt;
+    if (this.x > 600)
 
- var enemy1 = new Enemy(-3, makeRandomY(), makeRandomSpeed());
- var enemy2 = new Enemy(-3, makeRandomY(), makeRandomSpeed());
- var enemy3 = new Enemy(-3, makeRandomY(), makeRandomSpeed());
- var enemy4 = new Enemy(-3, makeRandomY(), makeRandomSpeed());
-
- var allEnemies = [enemy1, enemy2, enemy3, enemy4];
-
- console.log(enemy1);
+    {
+        this.reset();
+    }
+};
 
 
- // Now write your own player class
 
- //var PlayerYArray = [60,145,230,315,400];
-
- // Player object
-
-
- var Player = function(x, y) {
-     this.x = x;
-     this.y = y;
-     this.sprite = 'images/char-boy.png';
-
- };
-
- Player.prototype.render = function() {
-     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
- };
-
- Player.prototype.reset = function() {
-     this.x = 200;
-     this.y = 400;
- };
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
- // This class requires an update(), render() and
- // a handleInput() method.
 
+var allEnemies = [];
+for (var i = 0; i < 5; i++) {
+    var enemy = new Enemy(-3, makeRandomY(), makeRandomSpeed());
+    allEnemies.push(enemy);
+};
+
+
+
+// Now write your own player class
+
+
+// Player object
+
+
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-boy.png';
+
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 400;
+};
+
+
+// This class requires an update(), render() and
+// a handleInput() method.
 
 
 
 
+Player.prototype.update = function() {
 
- Player.prototype.update = function() {
+    if (this.y == -25) {
+        //reset if player on water
+        this.reset();
+    } else if (this.y > 400)
+    //player can not go lower than starting position
+    {
+        this.y = 400;
+    } else if (this.x > 450)
+    //if the player passed 450 on the right side, let player appear again on left side
+    {
+        this.x = -50;
+    } else if (this.x < -50)
+    //if the player passes - 50 on the left side, let player appear again on right side
+    {
+        this.x = 450;
+    } else {
 
-     if (this.y == -25) {
-         //reset if player on water
-         this.reset();
-     } else if (this.y > 400)
-     //player can not go lower than starting position
-     {
-         this.y = 400;
-     } else if (this.x > 500)
-     //let player appear again on left side
-     {
-         this.x = -100;
-     } else if (this.x < -100)
-     //let player appear again on right side
-     {
-         this.x = 500;
-     } else if (this.y >= 60 && this.y <= 230) {
+        for (var i = 0; i < allEnemies.length; i++) {
 
-         for (var i = 0; i < allEnemies.length; i++) {
+            if ((allEnemies[i].x > this.x - 16 && allEnemies[i].x < this.x + 25) && (allEnemies[i].y == this.y)) {
+                //Check if the enemy is on the same x (range added) and y coordinates as the player
+                this.reset();
+            }
+        }
 
-
-             if (allEnemies[i].x > this.x - 16 && allEnemies[i].x < this.x + 25) {
-
-
-                 //if (allEnemies[i].x == this.x) {
-                 this.reset();
-             }
-         }
-
-     }
- };
-
-
- Player.prototype.reset = function() {
-     this.x = 200;
-     this.y = 400;
- };
-
- Player.prototype.handleInput = function(key) {
-     if (key === 'up') {
-         this.y -= 85;
-         console.log("Up");
-     } else if (key === 'down') {  
-         this.y += 85;
-         console.log("Down");
-     } else if (key === 'left') {
-         this.x -= 50;
-         console.log("Left");
-     } else if (key === 'right') {
-         this.x += 50;
-         console.log("Right");
-     }
- };
-
-
- document.addEventListener('keyup', function(e) {
-     var allowedKeys = {
-         37: 'left',
-         38: 'up',
-         39: 'right',
-         40: 'down'
-     };
-
-     player.handleInput(allowedKeys[e.keyCode]);
- });
+    }
+};
 
 
 
+Player.prototype.handleInput = function(key) {
+    if (key === 'up') {
+        this.y -= 85;
+        console.log("Up");
+    } else if (key === 'down') {
+        this.y += 85;
+        console.log("Down");
+    } else if (key === 'left') {
+        this.x -= 50;
+        console.log("Left");
+    } else if (key === 'right') {
+        this.x += 50;
+        console.log("Right");
+    }
+};
 
- // Place the player object in a variable called player
+
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
 
 
- var player = new Player(200, 400);
+// Place the player object in a variable called player
+
+var player = new Player(200, 400);
